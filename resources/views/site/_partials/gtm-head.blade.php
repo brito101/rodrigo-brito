0ea29ffd877bdfe5d1a1c7a2427b02eae7bc4046
brito101 <rodrigobrito101@hotmail.com>
2024-08-28 16:39:05 -0300

    mod: gtag

--- resources/views/site/_partials/gtm-head.blade.php
+++ resources/views/site/_partials/gtm-head.blade.php
@@ -1,16 +1,18 @@
 <!-- Google Tag Manager -->
-
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-T7BS7L85D2"></script>
 <script>
-    window.dataLayer = window.dataLayer || [];
-
-    function gtag() {
-        dataLayer.push(arguments);
-    }
-    gtag('js', new Date());
-
-    gtag('config', 'G-T7BS7L85D2', {
-        'cookieFlags': 'SameSite=None; Secure'
-    });
+    (function(w, d, s, l, i) {
+        w[l] = w[l] || [];
+        w[l].push({
+            'gtm.start': new Date().getTime(),
+            event: 'gtm.js'
+        });
+        let f = d.getElementsByTagName(s)[0],
+            j = d.createElement(s),
+            dl = l !== 'dataLayer' ? '&l=' + l : '';
+        j.async = true;
+        j.src =
+            'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+        f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-KBGXKNM');
 </script>
+<!-- End Google Tag Manager -->
